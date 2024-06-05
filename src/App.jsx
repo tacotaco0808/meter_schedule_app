69d5f84738958891484fc3b290eb99ff009badf7
takutou <takuto1488@gmail.com>
2024-06-05 22:55:19 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { ScheduleTree } from "./componetns/ScheduleTree";
+
+function App() {
+  return (
+    <>
+      <ScheduleTree />
+    </>
+  );
+}
+
+export default App;
---
+++ src/componetns/ScheduleTree.jsx
@@ -0,0 +1,3 @@
+export const ScheduleTree = () => {
+  return <>tese</>;
+};
